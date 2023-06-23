61d0908557e34bf2aea55c37ffb729399785324e
Tanim <tchoud227@gmail.com>
2023-06-23 01:23:01 -0400

    Fixed celsius values and added error message for invalid place

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ import streamlit as st
 import plotly.express as px
 from backend import get_data
 
+st.set_page_config(layout="wide")
 filtered_data = []
 # Add title, text input, slider, selectbox, and subheader
 st.title("Weather Forecast for the Next Days")
-place = st.text_input("Place: ")
+place = st.text_input("Place: ", )
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view",
@@ -14,19 +15,25 @@ st.subheader(f"{option} for the next {days} days in {place}")
 
 if place:
     # Get the temperature/sky data
-    filtered_data = get_data(place, days)
+    try:
+        filtered_data = get_data(place, days)
 
-    if option == "Temperature":
-        # Extract Data
-        temperatures = [dict["main"]["temp"] for dict in filtered_data]
-        dates = [dict["dt_txt"] for dict in filtered_data]
+        if option == "Temperature":
+            # Extract Data
+            temperatures = [float(dict["main"]["temp"] / 10 ) for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
 
-        # Create a temperature plot
-        figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
-        st.plotly_chart(figure)
-    if option == "Sky":
-        sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
-        print(sky_condition)
-        for condition in sky_condition:
-            st.image(image=f"images\{condition.lower()}.png", width=115)
+            # Create a temperature plot
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature (C)"})
+            st.plotly_chart(figure)
+        if option == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/clouds.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_path = [images[condition] for condition in sky_condition]
+            print(sky_condition)
+            st.image(image=image_path, width=100)
+
+    except KeyError:
+        st.write(f"{place} does not exist!")
 
